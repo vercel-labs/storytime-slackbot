1b08f7ed3c4c0ccce25a560dc43f34e75db4c42e
JJ Kasper <jj@jjsweb.site>
2025-09-29 15:19:04 -0700

    bump

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	/* config options here */
+	experimental: {
+		serverMinification: false
+	}
 };
 
 export default withWorkflow(nextConfig);
